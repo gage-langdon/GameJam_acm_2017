305b6adbb69b0a4faf74d40a64dd73e2cbfd76b6
Joey Simas <Joey@Joeys-MacBook-Pro.local>
2017-10-14 13:30:30 -0700

    Adding materials, plays space, fence script.

---
+++ Assets/Scripts/fence.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fence : MonoBehaviour {
+	
+	public GameObject model;
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		health = 3; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health > 0) {
+			model.SetActive (true); 
+			health = 3; 
+		}
+
+		if (health <= 0) {
+			model.SetActive (false); 
+		}
+	}
+}
