517a2842c8107bb80744f4350f0aaabcbcbae052
Joey Simas <Joey@Joeys-MacBook-Pro.local>
2017-10-14 16:17:14 -0700

    Pinata

---
+++ Assets/pinata.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pinata : MonoBehaviour {
+
+	int health; 
+	public float speed; 
+
+	// Use this for initialization
+	void Start () {
+		health = 3; 
+		speed = 1; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * Time.deltaTime * speed; 
+	}
+
+	void OnCollisionEnter(){
+			speed = 0;
+	}
+
+	void OnHit(){
+		health = health--; 
+		if (health <= 0) {
+			Destroy (gameObject); 
+		}
+	}
+}
